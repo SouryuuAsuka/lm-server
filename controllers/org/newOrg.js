51059e4450f4617034621139e7cc223f961ff258
SouryuuAsuka <asukavswar@gmail.com>
2023-04-04 16:35:51 +0300

    modified:   controllers/org/newOrg.js

--- controllers/org/newOrg.js
+++ controllers/org/newOrg.js
@@ -21,7 +21,25 @@ exports.newOrg = async (req, res) => {
             if (err) {
                 return res.status(401).json({ "error": true, "message": 'Unauthorized access.' });
             } else {
-                if (validator.isEmpty(req.body.name)) {
+                if (req.body.name == undefined) {
+                    return res.status(400).json({ success: false, error: "Название организации должно быть заполнено" })
+                }
+                else if (req.body.about == undefined) {
+                    return res.status(400).json({ success: false, error: "Описание организации должно быть заполнено" })
+                }
+                else if (req.body.category == undefined) {
+                    return res.status(400).json({ success: false, error: "Категория организации должно быть заполнено" })
+                }
+                else if (req.body.city == undefined) {
+                    return res.status(400).json({ success: false, error: "Город организации должно быть заполнено" })
+                }
+                else if (req.body.preparTimeMin == undefined) {
+                    return res.status(400).json({ success: false, error: "Минимальное время изготовления товаров организации должно быть заполнено" })
+                }
+                else if (req.body.preparTimeMax == undefined) {
+                    return res.status(400).json({ success: false, error: "Максимальное время изготовления товаров организации должно быть заполнено" })
+                }
+                else if (validator.isEmpty(req.body.name)) {
                     return res.status(400).json({ success: false, error: "Название организации должно быть заполнено" })
                 }
                 else if (validator.isEmpty(req.body.about)) {
@@ -30,11 +48,14 @@ exports.newOrg = async (req, res) => {
                 else if (validator.isEmpty(req.body.category)) {
                     return res.status(400).json({ success: false, error: "Тип организации должен быть указан" })
                 }
+                else if (validator.isEmpty(req.body.city)) {
+                    return res.status(400).json({ success: false, error: "Город организации должен быть указан" })
+                }
                 else if (validator.isEmpty(req.body.preparTimeMin)) {
                     return res.status(400).json({ success: false, error: "Минимальное время изготовления товаров организации должен быть указано" })
                 }
                 else if (validator.isEmpty(req.body.preparTimeMax)) {
-                    return res.status(400).json({ success: false, error: "Минимальное время изготовления товаров ип организации должен быть указано" })
+                    return res.status(400).json({ success: false, error: "Максимальное время изготовления товаров организации должен быть указано" })
                 }
                 else if (validator.isEmpty(req.body.country)) {
                     return res.status(400).json({ success: false, error: "Страна должна быть указана" })
