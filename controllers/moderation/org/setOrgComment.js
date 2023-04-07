0e48d7f277d93dff220a18bde8150f5f1b01cc6d
SouryuuAsuka <asukavswar@gmail.com>
2023-04-08 02:54:00 +0300

    modified:   controllers/moderation/org/setOrgComment.js

--- controllers/moderation/org/setOrgComment.js
+++ controllers/moderation/org/setOrgComment.js
@@ -23,25 +23,31 @@ exports.setOrgComment = async (req, res) => {
                         if (userRow.rows[0].user_role == 5 || userRow.rows[0].user_role == 6) {
                             pool.query(
                                 `UPDATE organizations_request SET moderator_comment = $1 WHERE org_id = $2;`, [req.body.comment, req.body.orgId], (err, orgRow) => {
-                                if (err) {
-                                    return res.status(500).json({ error: "Ошибка при обработке запроса" });
-                                } else {
-                                    var msgText = "Ваша заявка на создание организации обновлена!\nПолучен комментарий от модератора:\n"+req.body.comment  ;
-                                    cbot.post('sendmsg', {
-                                        key: process.env.CBOT_ACCESS_KEY,
-                                        id: userRow.rows[0].app_id,
-                                        text: msgText
-                                    })
-                                    .then(() => {
-                                        console.log('Removed the object')
-                                        return res.status(200).json({});
-                                    })
-                                    .catch(function (error) {
-                                        console.log(error)
-                                        return res.status(500).json({ error: "Ошибка при отправке сообщения в бот" });
-                                    })
-                                }
-                            });
+                                    if (err) {
+                                        return res.status(500).json({ error: "Ошибка при обработке запроса" });
+                                    } else {
+                                        if (req.body.comment != "") {
+                                            var msgText = "Ваша заявка на создание организации обновлена!\nПолучен комментарий от модератора:\n" + req.body.comment;
+                                            cbot.post('sendmsg', {
+                                                key: process.env.CBOT_ACCESS_KEY,
+                                                id: userRow.rows[0].app_id,
+                                                text: msgText
+                                            })
+                                                .then(() => {
+                                                    console.log('Removed the object')
+                                                    return res.status(200).json({});
+                                                })
+                                                .catch(function (error) {
+                                                    console.log(error)
+                                                    return res.status(500).json({ error: "Ошибка при отправке сообщения в бот" });
+                                                })
+
+                                        } else {
+                                            return res.status(200).json({});
+                                        }
+
+                                    }
+                                });
                         } else {
                             return res.status(500).json({
                                 error: "Недостаточно прав для совершения операции", //Database connection error
