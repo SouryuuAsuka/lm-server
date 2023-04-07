a0a3cd6dc7225bf9f6ce3004694da5e864dfc50e
SouryuuAsuka <asukavswar@gmail.com>
2023-04-08 02:58:10 +0300

    modified:   controllers/getProfile.js

--- controllers/getProfile.js
+++ controllers/getProfile.js
@@ -24,14 +24,17 @@ exports.getProfile = async (req, res) => {
                 }
                 if (!userRow.rows[0].telegram){
                     var salt = userRow.rows[0].pass_salt;
-                    user.tgCode = salt.substring(salt.length - 6) 
+                    user.tgCode = salt.substring(salt.length - 6)
+                    return res.status(200).json({ profile: user });
                 } else{
                     const tgUserRow = await pool.query(`SELECT * FROM tg_users WHERE user_id = $1`, [userRow.rows[0].user_id]);
                     if (tgUserRow.rows[0] != undefined) {
                         user.telegramUsername = tgUserRow.rows[0].username;
+                        return res.status(200).json({ profile: user });
+                    } else{
+                        return res.status(500).json({ error: true, message: 'Ошибка при поиске пользователя' });
                     }
                 }
-                return res.status(200).json({ profile: user });
             } else {
                 return res.status(500).json({ error: true, message: 'Пользователь не найден' });
             }
