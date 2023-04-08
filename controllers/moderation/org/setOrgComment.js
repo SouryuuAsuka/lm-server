d9692f583fd82d1a9f9ea66172d9dcfc0c8ed7eb
SouryuuAsuka <asukavswar@gmail.com>
2023-04-08 03:22:34 +0300

    modified:   controllers/moderation/org/setOrgComment.js

--- controllers/moderation/org/setOrgComment.js
+++ controllers/moderation/org/setOrgComment.js
@@ -8,51 +8,50 @@ exports.setOrgComment = async (req, res) => {
             console.log("начало проверки")
             if (err) {
                 return res.status(401).json({ error: true, message: 'Unauthorized access.' });
-            } else if (req.body.requestId == undefined) {
-                return res.status(401).json({ error: true, message: 'Unauthorized access.' });
+            } else if (req.body.orgId == undefined) {
+                return res.status(401).json({ error: true, message: 'Id организации не задан' });
+            } else if (decoded.userRole != 5 || decoded.userRole != 6) {
+                return res.status(401).json({ error: true, message: 'Недостаточно прав для редактирования оранизации' });
             } else {
                 pool.query(`
                     SELECT * 
-                    FROM users AS u
+                    FROM organizations_request AS o
+                    LEFT JOIN users AS u
+                    ON o.owner = u.user.id
                     LEFT JOIN tg_users AS t
                     ON u.user_id = t.user.id
-                    WHERE user_id = $1`, [decoded.userId], async (err, userRow) => {
+                    WHERE o.owner = $1`, [req.body.orgId], async (err, userRow) => {
                     if (err) {
                         return res.status(500).json({ error: "Ошибка при обработке запроса" });
                     } else {
-                        if (userRow.rows[0].user_role == 5 || userRow.rows[0].user_role == 6) {
-                            pool.query(
-                                `UPDATE organizations_request SET moderator_comment = $1 WHERE org_id = $2;`, [req.body.comment, req.body.orgId], (err, orgRow) => {
-                                    if (err) {
-                                        return res.status(500).json({ error: "Ошибка при обработке запроса" });
-                                    } else {
-                                        if (req.body.comment != "") {
-                                            var msgText = "Ваша заявка на создание организации обновлена!\nПолучен комментарий от модератора:\n" + req.body.comment;
-                                            cbot.post('sendmsg', {
-                                                key: process.env.CBOT_ACCESS_KEY,
-                                                id: userRow.rows[0].app_id,
-                                                text: msgText
+                        pool.query(
+                            `UPDATE organizations_request SET moderator_comment = $1 WHERE org_id = $2;`, [req.body.comment, req.body.orgId], (err, orgRow) => {
+                                if (err) {
+                                    return res.status(500).json({ error: "Ошибка при обработке запроса" });
+                                } else {
+                                    if (req.body.comment != "") {
+                                        var msgText = "Ваша заявка на создание организации обновлена!\nПолучен комментарий от модератора:\n" + req.body.comment;
+                                        cbot.post('sendmsg', {
+                                            key: process.env.CBOT_ACCESS_KEY,
+                                            id: userRow.rows[0].app_id,
+                                            text: msgText
+                                        })
+                                            .then(() => {
+                                                console.log('Removed the object')
+                                                return res.status(200).json({});
                                             })
-                                                .then(() => {
-                                                    console.log('Removed the object')
-                                                    return res.status(200).json({});
-                                                })
-                                                .catch(function (error) {
-                                                    console.log(error)
-                                                    return res.status(500).json({ error: "Ошибка при отправке сообщения в бот" });
-                                                })
+                                            .catch(function (error) {
+                                                console.log(error)
+                                                return res.status(500).json({ error: "Ошибка при отправке сообщения в бот" });
+                                            })
 
-                                        } else {
-                                            return res.status(200).json({});
-                                        }
-
+                                    } else {
+                                        return res.status(200).json({});
                                     }
-                                });
-                        } else {
-                            return res.status(500).json({
-                                error: "Недостаточно прав для совершения операции", //Database connection error
-                            });
-                        }
+
+                                }
+                            }
+                        );
                     }
                 })
             }
