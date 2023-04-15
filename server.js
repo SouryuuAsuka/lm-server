d0bf8ab1dbff2e97d327092690ae19aa41bfe4ed
SouryuuAsuka <asukavswar@gmail.com>
2023-04-15 15:35:24 +0300

    Changes to be committed: 	modified:   server.js

--- server.js
+++ server.js
@@ -83,8 +83,8 @@ app.use(require('./middleware/auth'));
 app.get('/api/profile', getProfile);
 app.get('/api/profile/orgs', getProfileOrgList);
 
-app.post('/api/org/good', uploadAvatar.single('avatar'), newGood);
-app.post('/api/org/good/edit', uploadAvatar.single('avatar'), editGood);
+app.post('/api/org/good', uploadAvatar.single('picture'), newGood);
+app.post('/api/org/good/edit', uploadAvatar.single('picture'), editGood);
 app.post('/api/org/good/active', setActive);
 
 app.post('/api/org', uploadAvatar.single('avatar'), newOrg);
