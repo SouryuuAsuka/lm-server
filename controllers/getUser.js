96aca26ddc8e18e3c41a8ef6735dfa2d0a6ca094
SouryuuAsuka <asukavswar@gmail.com>
2023-04-19 19:24:53 +0300

    modified:   controllers/getUser.js

--- controllers/getUser.js
+++ controllers/getUser.js
@@ -28,6 +28,19 @@ exports.getUser = async (req, res) => {
                     } else {
                         user.avatar = "defaultAvatar1";
                     }
+                    if (!userRow.rows[0].tech_telegram){
+                        var salt = userRow.rows[0].pass_salt;
+                        user.tgCode = salt.substring(salt.length - 6)
+                        return res.status(200).json({ profile: user });
+                    } else{
+                        const tgUserRow = await pool.query(`SELECT * FROM tg_tech_users WHERE user_id = $1`, [userRow.rows[0].user_id]);
+                        if (tgUserRow.rows[0] != undefined) {
+                            user.telegramUsername = tgUserRow.rows[0].username;
+                            return res.status(200).json({ profile: user });
+                        } else{
+                            return res.status(500).json({ error: true, message: 'Ошибка при поиске пользователя' });
+                        }
+                    }
                     return res.status(200).json({user: user});
                 } else {
                     return res.status(401).json({ error: true, message: 'Unauthorized access.' });
