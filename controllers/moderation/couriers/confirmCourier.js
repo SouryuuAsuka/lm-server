b9b8eee61392a25f26ebf429bac81cda2d7d20c2
SouryuuAsuka <asukavswar@gmail.com>
2023-06-04 19:28:34 +0300

    modified:   controllers/moderation/couriers/confirmCourier.js

--- controllers/moderation/couriers/confirmCourier.js
+++ controllers/moderation/couriers/confirmCourier.js
@@ -25,7 +25,7 @@ exports.confirmCourier = async (req, res) => {
                         var msgText = "Ваша заявка на регистрацию в курьерской системе одобрена."
                         dbot.post('sendmsg', {
                             key: process.env.DBOT_ACCESS_KEY,
-                            id: tgRow.rows[0],
+                            id: tgRow.rows[0].app_id,
                             text: msgText
                         })
                         return res.status(200).json({});
