e187394315bea557d614bbd7af81e4b46471a00a
SouryuuAsuka <asukavswar@gmail.com>
2023-06-04 21:49:18 +0300

    modified:   controllers/moderation/couriers/getCouriers.js

--- controllers/moderation/couriers/getCouriers.js
+++ controllers/moderation/couriers/getCouriers.js
@@ -10,49 +10,54 @@ exports.getCouriers = async (req, res) => {
             } else if (decoded.userRole != 5 && decoded.userRole != 6) {
                 return res.status(401).json({ error: true, message: 'Недостаточно прав для редактирования оранизации' });
             } else {
-                var count = pool.query("SELECT COUNT(*) FROM tg_couriers WHERE confirm = false", [])
-                console.log(JSON.stringify(count))
-                if (count != 0) {
-                    pool.query(`
+                pool.query("SELECT COUNT(*) FROM tg_couriers WHERE confirm = false", [], (err, couriersRow) => {
+                    if (err) {
+                        return res.status(401).json({ error: true, message: 'Unauthorized access.' });
+                    } else {
+                        console.log(JSON.stringify(count))
+                        if (count != 0) {
+                            pool.query(`
                         SELECT 
                         tg_id, username, country, city, firstname, lastname
                         FROM tg_couriers
                         WHERE confirm = false`, [], (err, couriersRow) => {
-                        if (err) {
-                            console.log(err)
-                            return res.status(500).json({ error: 'Ошибка поиска' });
+                                if (err) {
+                                    console.log(err)
+                                    return res.status(500).json({ error: 'Ошибка поиска' });
+                                } else {
+                                    var couriersList = [];
+
+                                    for (let i = 0; i < couriersRow.rows.length; i++) {
+                                        var country;
+                                        var city;
+                                        if (couriersRow.rows[i].country == "ge") {
+                                            country = "Грузия";
+                                        }
+                                        if (couriersRow.rows[i].city == "tbi") {
+                                            city = "Тбилиси"
+                                        }
+                                        couriersList.push({
+                                            tgId: couriersRow.rows[i].tg_id,
+                                            username: couriersRow.rows[i].username,
+                                            firstname: couriersRow.rows[i].firstname,
+                                            lastname: couriersRow.rows[i].lastname,
+                                            country: country,
+                                            city: city
+                                        })
+                                        if (i + 1 == couriersRow.rows.length) {
+                                            return res.status(200).json({ couriers: couriersList, count: count });
+                                        }
+
+                                    }
+
+                                }
+                            });
                         } else {
-                            var couriersList = [];
-
-                            for (let i = 0; i < couriersRow.rows.length; i++) {
-                                var country;
-                                var city;
-                                if (couriersRow.rows[i].country == "ge") {
-                                    country = "Грузия";
-                                }
-                                if (couriersRow.rows[i].city == "tbi") {
-                                    city = "Тбилиси"
-                                }
-                                couriersList.push({
-                                    tgId: couriersRow.rows[i].tg_id,
-                                    username: couriersRow.rows[i].username,
-                                    firstname: couriersRow.rows[i].firstname,
-                                    lastname: couriersRow.rows[i].lastname,
-                                    country: country,
-                                    city: city
-                                })
-                                if (i + 1 == couriersRow.rows.length) {
-                                    return res.status(200).json({ couriers: couriersList, count: count });
-                                }
-
-                            }
-
+                            console.log("Организаций не найдено")
+                            return res.status(200).json({ couriers: [], count: 0 });
                         }
-                    });
-                } else {
-                    console.log("Организаций не найдено")
-                    return res.status(200).json({ couriers: [], count: 0 });
-                }
+                    }
+                })
             }
         })
     }
