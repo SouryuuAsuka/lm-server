6a89b1a7298768d6d16466859bad228f1014ffe3
Asuka <asuka@>
2023-06-17 15:04:17 +0300

    modified:   controllers/org/getOrg.js

--- controllers/org/getOrg.js
+++ controllers/org/getOrg.js
@@ -48,10 +48,10 @@ exports.getOrg = async (req, res) => {
                     ON g.org_id = org.org_id
                     WHERE org.org_id = $1 AND (qu.status_code = 5 AND qu.paid = false)
                     GROUP BY org.org_id
-                    ORDER BY g.created DESC`,
+                    ORDER BY org.created DESC`,
                     [req.query.id], async (err, orgRow) => {
                         if (err) {
-                            console.log(err)
+                            consoe.log(err)
                             return res.status(400).json({ success: false, error: "Произошла ошибка при верификации запроса" })
                         } else {
                             if (orgRow.rows.length != 0) {
