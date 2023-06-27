b117c91b0c82cd293bd5170409882721247deea9
Asuka <asuka@>
2023-06-27 13:42:42 +0300

    modified:   controllers/org/getOrg.js

--- controllers/org/getOrg.js
+++ controllers/org/getOrg.js
@@ -79,11 +79,13 @@ function sendOrgData(res, org, owner) {
         if (i + 1 == org.goods.length) {
             if (owner) {
                 var totalSum = 0
-                totalSum += Number(org.quests.map((quest) => {
-                    return quest.goods.map((good) => {
-                        return Number(good.num) * Number(good.price)
-                    })
-                }))
+                if (org.quests.length >0){
+                    totalSum += Number(org.quests.map((quest) => {
+                        return quest.goods.map((good) => {
+                            return Number(good.num) * Number(good.price)
+                        })
+                    }))
+                }
                 newOrg.usd_total = totalSum;
                 return res.status(200).json({ org: newOrg });
             } else {
