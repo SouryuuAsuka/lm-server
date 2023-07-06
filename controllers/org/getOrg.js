5f61f3ebf3354dcef718811d9a59154e9e726ff8
Asuka <asuka@>
2023-07-06 07:01:39 +0300

    modified:   controllers/org/getOrg.js

--- controllers/org/getOrg.js
+++ controllers/org/getOrg.js
@@ -84,7 +84,7 @@ function sendOrgData(res, org, owner) {
                         goodSum+= Number(quest.goods.map((good) => {
                             console.log("goodSum - "+goodSum)
                             console.log("good.num - "+good.num + ", good.price - "+good.price)
-                            return Number(good.num) * Number(good.price)
+                            return (Number(good.num) * Number(good.price))
                         }))
                         console.log("totalSum - "+totalSum)
                         return goodSum
