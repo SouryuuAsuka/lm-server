4b6d9e60c02c52e8ee47f30740960386db4e7d76
Asuka <asuka@>
2023-08-20 02:01:09 +0300

    api6

--- app.js
+++ app.js
@@ -23,13 +23,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(cookies());
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization");
-  next();
-});
 app.use(cors());
 
 // Позволяет видеть в req.ip реальный ip пользователя, а не nginx
