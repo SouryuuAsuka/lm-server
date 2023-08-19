ada069e4f556d56d2d8c426dd4ad23cd247cee7a
Asuka <asuka@>
2023-08-20 02:39:27 +0300

    api fix 9

--- app.js
+++ app.js
@@ -24,11 +24,16 @@ app.use(bodyParser.json());
 
 app.use(cookies());
 app.use(cors({
-  origin: '*',
+  origin: ['https://lampymarket.com', 'https://api.lampymarket.com', 'https://cbot.lampymarket.com', 'https://tbot.lampymarket.com', 'https://dbot.lampymarket.com'],
   credentials: true,
   optionsSuccessStatus: 200   
 }));
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 // Позволяет видеть в req.ip реальный ip пользователя, а не nginx
 app.set('trust proxy', true);
 
