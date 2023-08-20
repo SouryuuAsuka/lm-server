3837ecbf73085f9968b32cdefb699eaec776fec5
Asuka <asuka@>
2023-08-20 22:24:03 +0300

    cors bugfix 20.08 2

--- app.js
+++ app.js
@@ -28,12 +28,12 @@ app.use(cors({
   credentials: true,
   optionsSuccessStatus: 200   
 }));
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
-});
+});*/
 // Позволяет видеть в req.ip реальный ip пользователя, а не nginx
 app.set('trust proxy', true);
 
