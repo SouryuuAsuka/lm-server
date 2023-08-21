61d2bdbce4053aba7232a68563374584812110e5
Asuka <asuka@>
2023-08-22 00:26:22 +0300

    clean arch test 10

--- src/adapters/controllers/org/org.controller.js
+++ src/adapters/controllers/org/org.controller.js
@@ -5,6 +5,7 @@ module.exports = (dependecies) => {
   const getOrgList = (req, res, next) => {
     const GetOrgListCommand = GetOrgList(orgRepository);
     GetOrgListCommand(req.query.p, req.query.c, req.query.t).then((response) => {
+      console.log(JSON.stringify(response));
       res.json(response);
     }).catch((err) => {
       next(err);
