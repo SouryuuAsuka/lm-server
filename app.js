af972a31e8c90189625878f7fca426d656e5df23
Asuka <asuka@>
2023-08-27 01:12:29 +0300

    clean architect bugfix 12

--- app.js
+++ app.js
@@ -19,8 +19,14 @@ const ErrorHandler = require('@common/ErrorHandler');
 const dependencies = require("@config/projectDependencies");
 
 const app = express();
-app.use(cors())
-app.use(function(req, res, next) {
+const corsOptions = {
+  origin: '*',
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
